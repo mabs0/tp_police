5d45a5b472e9e07d4be55de1509f68e372b02ea2
mabs0 <118767637+mabs0@users.noreply.github.com>
2024-03-17 11:13:56 +0100

    Create fonctions.h

---
+++ fonctions.h
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_I 1000
+
+#ifndef TP_ETUDE_DE_CAS_FONCTIONS_H
+#define TP_ETUDE_DE_CAS_FONCTIONS_H
+void start_timer();
+void stop_timer();
+void tri_bulle(char **liste, int taille);
+void tri_insertion(char **liste, int taille);
+#endif //TP_ETUDE_DE_CAS_FONCTIONS_H
