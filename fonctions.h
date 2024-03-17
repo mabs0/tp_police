3553a4dca04823c7a28334494adee943c2d81fee
HugoMBKC <149174933+HugoMBKC@users.noreply.github.com>
2024-03-17 17:51:59 +0100

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <time.h>
 #include <stdio.h>
@@ -7,10 +6,16 @@
 
 #define MAX_I 1000
 
+typedef struct comptageHistorigramme {
+    char nom;
+    int apparitions;
+} t_comptageHistorigramme;
+
 #ifndef TP_ETUDE_DE_CAS_FONCTIONS_H
 #define TP_ETUDE_DE_CAS_FONCTIONS_H
 void start_timer();
 void stop_timer();
 void tri_bulle(char **liste, int taille);
 void tri_insertion(char **liste, int taille);
+void historigramme (char **liste);
 #endif //TP_ETUDE_DE_CAS_FONCTIONS_H
